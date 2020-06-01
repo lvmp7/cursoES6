5e1bb43de1587ae225a9b90f4dff1caa47824107
7700261670 <teste@21>
2020-05-31 22:55:29 -0300

    movimentacao de pasta

---
+++ Modulo1/Aula2/bundle.js
@@ -0,0 +1,31 @@
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(data);
+  }
+
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = "Vinicius";
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+
+}
+
+const MinhaTodoList = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+  MinhaTodoList.add('Novo Todo');
+};
+
+MinhaTodoList.mostraUsuario();
